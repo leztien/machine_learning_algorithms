e69245abd19b0f20dfaac4975044c8cce505dc8c
Ilja Neitzel <48551497+leztien@users.noreply.github.com>
2024-03-12 23:12:55 +0100

    Update classifiers_errors_visualization.py

--- classifiers_errors_visualization.py
+++ classifiers_errors_visualization.py
@@ -2,10 +2,10 @@
 """
 Make a plot that enables you to compare visually
 how different the erros that your models make.
-
 Based on the 'missingno' library
 """
 
+import warnings
 from collections import Counter
 import numpy as np, pandas as pd
 import matplotlib.pyplot as plt
@@ -20,7 +20,10 @@ from sklearn.metrics import accuracy_score
 def plot_errors(X, y, X_val=None, y_val=None, estimators=[], scorer=None, 
                 figsize=None, random_state=None):
     """
-    ...
+    Make a plot that enables you to compare visually
+    how different the erros that your models make.
+    Based on the 'missingno' library
+    TODO: better docs
     """
     figsize = figsize or (10, 6)
     test_size = 0.2
@@ -28,31 +31,32 @@ def plot_errors(X, y, X_val=None, y_val=None, estimators=[], scorer=None,
         X, X_val, y, y_val = train_test_split(X, y, test_size=test_size, random_state=random_state)
     
     # fit
-    estimators = [est.fit(X, y) for est in estimators]
-    y_preds = [est.predict(X_val) for est in estimators]
-    scores = [scorer(y_val, y_pred) for y_pred in y_preds]
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", FutureWarning)
+        estimators = [est.fit(X, y) for est in estimators]
+        y_preds = [est.predict(X_val) for est in estimators]
+        scores = [scorer(y_val, y_pred) for y_pred in y_preds]
     
+    # Estimator names
     names = [(est[-1] if hasattr(est, 'steps')  else est).__class__.__name__ 
              for est in estimators]
     
-    # what if there are duplicate names of estimators
+    # What if there are duplicate names of estimators
     counter = Counter()
     for i,e in enumerate(names):
         counter.update([e])
         n = counter.get(e)
         names[i] = f"{e}{n if n > 1 else ''}"
-
+    
+    # Need more comments below
     df = pd.DataFrame(y_preds, index=names).T
     df = df.iloc[:, np.argsort(scores)[::-1]]
-    df['actual'] = y_val
+    df['actual'] = np.array(y_val)
     df.sort_values(['actual'] + list(df.columns[:-1]), inplace=True)
     class_borders = pd.DataFrame(Counter(y_val).items()).sort_values(0).cumsum().iloc[:-1, -1].values
-
     df = pd.DataFrame(df.values == df[['actual']].values, columns=df.columns).drop('actual', axis=1).replace({False: np.nan})
-    
     ax = msno.matrix(df, figsize=figsize)
     ax.hlines(y=class_borders, xmin=-0.5, xmax=len(estimators)-0.5, color='b')
-    ax.set_xticks(ticks=[0,1,2], labels=[10,20,30], minor=True)
     [ax.text(i, len(y_val)*1.05, round(s,2)) for i,s in enumerate(sorted(scores, reverse=True))]
     ax.set_title("Errors made by models.\ngrey=correct, white=false predictions\nthe horizontal lines separate the classes")
     ax.set_yticks([])
@@ -67,15 +71,22 @@ def make_data(m, n, k, random_state=None):
     return X,y
 
 
+
 # DEMO
 if __name__ == '__main__':
     random_state = None
-    X,y = make_data(1000, 5, k=3, random_state=None)
+    X,y = make_data(50, 5, k=3, random_state=None)
     
     md1 = GaussianNB()
     md2 = LogisticRegression()
     md3 = RandomForestClassifier(2, random_state=42)
     md4 = RandomForestClassifier(3, random_state=42)
+    
+    if np.random.random() > 0.5:
+        print("input: dataframe")
+        X,y = pd.DataFrame(X), pd.Series(y)
+    else:
+        print("input: ndarray")
     
     plot_errors(X,y, estimators=[md1, md2, md3, md4], scorer=accuracy_score)
     
